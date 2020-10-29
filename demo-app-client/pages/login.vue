5b7f9653b6d595b916f966e98edef0a200f560b3
hoangminh <le.minh.hoang@sun-asterisk.com>
2020-10-29 10:48:11 +0700

    test pr

--- demo-app-client/pages/login.vue
+++ demo-app-client/pages/login.vue
@@ -4,7 +4,7 @@
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-header">
-                        Login
+                        Login Now
                     </div>
                     <div class="card-body">
                         <form @submit.prevent="login">
