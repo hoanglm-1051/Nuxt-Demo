2cf3b7a339916590323b22f2af1136740e8cf640
Lê Minh Hoàng <70552893+hoanglm-2435@users.noreply.github.com>
2022-10-19 10:42:10 +0700

    Create test.vue

---
+++ web/pages/test.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        <div v-if="$auth.loggedIn">
+            <h1>Hello {{ user.name }}!</h1>
+            <div class="row">
+                <div class="col-md-6">
+                    <ListTask />
+                </div>
+                <div class="col-md-6">
+                    <CompleteTask />
+                </div>
+            </div>
+            <GetTask />
+        </div>
+        <h1 v-if="!$auth.loggedIn">
+            Please sign in!
+        </h1>
+    </div>
+</template>
