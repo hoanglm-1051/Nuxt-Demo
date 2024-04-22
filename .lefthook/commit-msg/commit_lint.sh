db9ea9ec4f292ea8cbb31672961a725ad7e952ef
hoanglm-2435 <le.minh.hoang@sun-asterisk.com>
2024-04-22 21:55:56 +0700

    FEAT-123: test

--- .lefthook/commit-msg/commit_lint.sh
+++ .lefthook/commit-msg/commit_lint.sh
@@ -1,7 +1,7 @@
 INPUT_FILE=$1
 START_LINE=`head -n1 $INPUT_FILE`
-PATTERN="^(TICKET)-[[:digit:]]+: "
+PATTERN="^(FEAT)-[[:digit:]]+: "
 if ! [[ "$START_LINE" =~ $PATTERN ]]; then
-  echo "Bad commit message, see example: TICKET-123: some text"
+  echo "Bad commit message, see example: FEAT-123: something"
   exit 1
 fi
